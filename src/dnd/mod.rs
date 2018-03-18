3d0f501a72518aee50c44434b19519cfbb2246cf
sireliah <sajuukk@gmail.com>
2018-03-19 00:04:22 +0100

    Create transparent window on the edge of the screen.

--- src/dnd/mod.rs
+++ src/dnd/mod.rs
@@ -1,4 +1,5 @@
 
+extern crate cairo;
 extern crate gdk;
 extern crate gio;
 extern crate gtk;
@@ -9,8 +10,10 @@ use std::thread;
 use std::sync::Arc;
 
 use std::path::Path;
-use self::gtk::{ContainerExt, BoxExt, GtkWindowExt, Inhibit, WidgetExtManual, WidgetExt, LabelExt};
+use self::gtk::{ApplicationWindow, ContainerExt, BoxExt, GtkWindowExt, Inhibit, WidgetExtManual, WidgetExt, LabelExt};
 use self::gio::{ApplicationExt, ApplicationExtManual};
+
+use self::gdk::{ScreenExt, WindowExt};
 
 use bluetooth;
 
@@ -32,7 +35,7 @@ fn spawn_send_job(file_path: &str) {
 pub fn build_window(application: &gtk::Application) -> Result<(), Box<Error>> {
     let targets = vec![gtk::TargetEntry::new("STRING", gtk::TargetFlags::OTHER_APP, 0),
                        gtk::TargetEntry::new("text/uri-list", gtk::TargetFlags::OTHER_APP, 0)];
-    let label = gtk::Label::new("Drop your file here");
+    let label = gtk::Label::new("D");
     label.drag_dest_set(gtk::DestDefaults::ALL, &targets, gdk::DragAction::COPY);
 
     label.connect_drag_data_received(|w, _, _, _, s, _, _| {
@@ -60,9 +63,19 @@ pub fn build_window(application: &gtk::Application) -> Result<(), Box<Error>> {
 
     // Finish populating the window and display everything
     let window = gtk::ApplicationWindow::new(application);
+
+    set_visual(&window, &None);
+    window.connect_screen_changed(set_visual);
+    window.connect_draw(draw);
+
     window.set_title("Dragit!");
-    window.set_default_size(600, 600);
+    window.set_default_size(5, 1000);
     window.add(&hbox);
+    window.set_app_paintable(true);
+    window.set_decorated(false);
+    window.set_skip_taskbar_hint(true);
+    window.move_(0, 0);
+    window.set_keep_above(true);
     window.show_all();
 
     let window_clone = window.clone();
@@ -73,6 +86,24 @@ pub fn build_window(application: &gtk::Application) -> Result<(), Box<Error>> {
         Inhibit(false)
     });
     Ok(())
+}
+
+
+fn set_visual(window: &ApplicationWindow, _screen: &Option<gdk::Screen>) {
+    if let Some(screen) = window.get_screen() {
+        if let Some(visual) = screen.get_rgba_visual() {
+            window.set_visual(&visual); // crucial for transparency
+        }
+    }
+}
+
+
+fn draw(_window: &ApplicationWindow, ctx: &cairo::Context) -> Inhibit {
+    // crucial for transparency
+    ctx.set_source_rgba(0.0, 0.0, 0.0, 0.4);
+    ctx.set_operator(cairo::enums::Operator::Screen);
+    ctx.paint();
+    Inhibit(false)
 }
 
 
