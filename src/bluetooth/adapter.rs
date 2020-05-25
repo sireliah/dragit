867cf1fd8854d96e5c4f81ec33efebc53019a207
sireliah <sajuukk@gmail.com>
2020-05-25 23:20:56 +0200

    Make bluetooth adapter compatible with recent blurz release.

--- src/bluetooth/adapter.rs
+++ src/bluetooth/adapter.rs
@@ -5,7 +5,7 @@ use std::process;
 use std::thread::sleep;
 use std::time::Duration;
 
-use self::blurz::bluetooth_obex::open_bus_connection;
+use self::blurz::bluetooth_session::BluetoothSession as Session;
 use self::blurz::bluetooth_obex::{
     BluetoothOBEXSession as OBEXSession, BluetoothOBEXTransfer as OBEXTransfer,
 };
@@ -19,29 +19,29 @@ pub struct BluetoothProtocol;
 impl Protocol for BluetoothProtocol {
     fn transfer_file(&self, file_path: &str) -> Result<(), Box<dyn Error>> {
         info!("Received file to transfer: '{}'", file_path);
-        let adapter: Adapter = Adapter::init().unwrap();
-        let devices: Vec<String> = adapter.get_device_list().unwrap();
+        let session = &Session::create_session(None)?;
+        let adapter: Adapter = Adapter::init(session)?;
+        let devices: Vec<String> = adapter.get_device_list()?;
 
         let filtered_devices = devices
             .iter()
             .filter(|&device_id| {
-                let device = Device::new(device_id.to_string());
-                device.is_ready_to_receive().unwrap()
+                let device = Device::new(session, device_id.to_string());
+                device.is_ready_to_receive().expect("Could not check device")
             })
             .cloned()
             .collect::<Vec<String>>();
 
         let device_id: &str = &filtered_devices.get(0).expect("No devices found!");
-        let device = Device::new(device_id.to_string());
+        let device = Device::new(session, device_id.to_string());
 
         match connect(&device) {
-            Ok(_) => match send_file_to_device(&device, file_path) {
+            Ok(_) => match send_file_to_device(session, &device, file_path) {
                 Ok(_) => info!("File sent to the device successfully."),
                 Err(err) => error!("{:?}", err),
             },
             Err(err) => error!("{:?}", err),
         }
-
         Ok(())
     }
 }
@@ -57,8 +57,8 @@ fn connect(device: &Device) -> Result<(), Box<dyn Error>> {
     );
     let uuids = device.get_uuids();
     info!("{:?}", uuids);
-    device.connect().ok();
-    let push_func_found: bool = uuids.unwrap().contains(&obex_push_uuid);
+    device.connect(10000).ok();
+    let push_func_found: bool = uuids?.contains(&obex_push_uuid);
     info!("Push file functionality found: {}", push_func_found);
     match device.is_connected() {
         Ok(_) => info!("Connected!"),
@@ -68,14 +68,15 @@ fn connect(device: &Device) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn send_file_to_device(device: &Device, file_path: &str) -> Result<(), Box<dyn Error>> {
-    let connection = open_bus_connection()?;
-    let session = OBEXSession::new(connection, device)?;
+fn send_file_to_device(session: &Session, device: &Device, file_path: &str) -> Result<(), Box<dyn Error>> {
+    let session = OBEXSession::new(session, device)?;
     let transfer = OBEXTransfer::send_file(&session, file_path)?;
 
     match transfer.wait_until_transfer_completed() {
         Ok(_) => info!("Ok"),
         Err(error) => error!("{:?}", error),
     }
+
+    session.remove_session()?;
     Ok(())
 }
