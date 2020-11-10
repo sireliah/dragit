0be2ecb7981e358274ac5de8ff1dedef3c5858b1
sireliah <sajuukk@gmail.com>
2020-11-10 22:14:52 +0100

    Use build script only for Windows.

--- build.rs
+++ build.rs
@@ -1,11 +1,8 @@
-//fn main() {
-//println!(r"cargo:rustc-link-search=native=C:\msys64\home\vieviurka\vcpkg\installed\x64-windows\lib");
-//}
-
-use autovcpkg;
+#[cfg(target_os = "windows")]
 fn main() {
     autovcpkg::configure(&["gtk", "cairo"]);
-    #[cfg(target_os = "windows")]
-    // vcpkg generate gtk adn gdk libraries with 3.0, where gtk-rs et al. expect only 3, we duplicate and rename them so rust will be able to find and link correctly
     autovcpkg::lib_fixup(&[("gtk-3.0.lib", "gtk-3.lib"), ("gdk-3.0.lib", "gdk-3.lib")]);
 }
+
+#[cfg(not(target_os = "windows"))]
+fn main() {}
