6f71aa306f2f04afe05ef18681a64e0b25b20f78
sireliah <sajuukk@gmail.com>
2021-03-21 22:04:32 +0100

    Try to build without autovcpkg.

--- build.rs
+++ build.rs
@@ -8,8 +8,8 @@ fn build_proto() {
 #[cfg(target_os = "windows")]
 fn main() {
     build_proto();
-    autovcpkg::configure(&["gtk", "cairo"]);
-    autovcpkg::lib_fixup(&[("gtk-3.0.lib", "gtk-3.lib"), ("gdk-3.0.lib", "gdk-3.lib")]);
+    // autovcpkg::configure(&["gtk", "cairo"]);
+    // autovcpkg::lib_fixup(&[("gtk-3.0.lib", "gtk-3.lib"), ("gdk-3.0.lib", "gdk-3.lib")]);
 }
 
 #[cfg(not(target_os = "windows"))]
