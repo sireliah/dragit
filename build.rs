be1e9b7faa399153a8f2df8f00f4411c28cd6270
sireliah <sajuukk@gmail.com>
2021-04-24 15:05:13 +0200

    Cleanup the release workflow.

--- build.rs
+++ build.rs
@@ -5,13 +5,6 @@ fn build_proto() {
     prost_build::compile_protos(&["src/p2p/transfer/metadata.proto"], &["src/"]).unwrap();
 }
 
-#[cfg(target_os = "windows")]
-fn main() {
-    build_proto();
-    println!("cargo:rustc-cdylib-link-arg=src/dragit.res");
-}
-
-#[cfg(not(target_os = "windows"))]
 fn main() {
     build_proto();
 }
