e11603433edf569443dcd75916f4f11e665814ef
sireliah <sajuukk@gmail.com>
2022-05-07 14:19:52 +0200

    Allow specifying the application name on startup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 // Uncomment to hide the terminal on Windows
 #![windows_subsystem = "windows"]
+use std::env;
+
 use env_logger::Env;
+use log::info;
 
 use dragit::dnd;
 
 fn main() {
     let env = Env::default().filter_or("LOG_LEVEL", "info");
+    let app_name = env::var("APPLICATION_NAME").unwrap_or("com.sireliah.Dragit".to_string());
+
     env_logger::init_from_env(env);
-    dnd::start_window();
+
+    info!("Starting {}", app_name);
+    dnd::start_window(app_name);
 }
